86a5e9b72e7bc4f2825586f1a08396860b222e6e
michaeljohncodes <michaelchukwunyem@gmail.com>
2025-08-27 16:59:04 +0100

    overflow-scroll

--- src/layouts/landing.page.layout/Floats.jsx
+++ src/layouts/landing.page.layout/Floats.jsx
@@ -10,7 +10,7 @@ import floatImage7 from ".././../assets/images/float-image7.png";
 const Floats = () => {
   return (
     <div className=" floats w-full mx-auto py-[10px] px-[50px] flex flex-col  items-center  max-[430px]:px-[10px]">
-      <main className=" w-full flex justify-between gap-x-[40px] items-center overflow-scroll py-[50px] max-[835px]:w-full max-[835px]:flex max-[835px]:overflow-scroll max-[1440px]:overflow-scroll max-[835px]:snap-start max-[835px]:gap-[20px] ">
+      <main className=" w-full flex justify-between gap-x-[100px] items-center overflow-scroll overflow-x-auto custom-scroll py-[50px] max-[835px]:w-full max-[835px]:flex max-[835px]:overflow-scroll max-[1440px]:overflow-scroll max-[835px]:snap-start max-[835px]:gap-[20px] ">
         <img src={floatImage1} alt="" className=" max-[430px]:max-w-[25%]" />
 
         <img src={floatImage2} alt="" className=" max-[430px]:max-w-[25%]" />
